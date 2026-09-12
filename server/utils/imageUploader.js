46139737568bd3ec3fc28696cc37f4f61fe1b88a
Bhar5399 <bharatrathod.work25@gmail.com>
2026-09-12 18:49:44 +0530

    Fix Cloudinary upload for dynamic folder mode

--- server/utils/imageUploader.js
+++ server/utils/imageUploader.js
@@ -1,7 +1,7 @@
 const cloudinary = require("cloudinary").v2
 
 exports.uploadImageToCloudinary = async (file, folder, height, quality) => {
-  const options = { folder }
+  const options = { asset_folder: folder, folder }
   if (height) {
     options.height = height
   }
